fcdae1b220951f67dbb3ae4fb968df6d0c894ab9
Louis Croce <ljc2154@columbia.edu>
2014-10-27 20:39:37 -0400

    commented

--- Recitation_8_ncat.c
+++ Recitation_8_ncat.c
@@ -15,37 +15,46 @@ int main(int argc, char **argv)
 
   if (argc != 2) {
     fprintf(stderr, "%s\n", "usage: ncat <file_name>");
-    exit(1);
+    return 1;
   }
   
   char *filename = argv[1];
   
+  // open's up the file filename as a FILE *, a data type used for reading/writing files
   FILE *fp = fopen(filename, "r");
   
+  // check if fopen returned NULL
   if (fp == NULL) {
+    // no file of name filename
     perror(filename);
-    exit(1);
+    return 1;
   }
   
+  // array to store chars from a given line
   char buf[100];
   
+  // keep track of line number because we want to print it!
   int lineno = 1;
   
+  // read file until either 100 bytes are read or a new line character/EOF character is read
   while (fgets(buf, sizeof(buf), fp) != NULL) {
   
+    // leave spacing as if the integer is 4 digits, but only fill in the right most digits as needed
     printf("%4d ", lineno++);
     
-    if (fputs(buf, stdout) == EOF) {
+    if (fputs(buf, stdout) == EOF) { // like printf
       perror("can’t write to stdout");
       exit(1);
     }
  }
 
- if (ferror(fp)) {
+ // check for error during reading file
+ if (ferror(fp)) { // checks if error flag of FILE * was set (?)
    perror(filename);
    exit(1);
  }
 
+ // we will not read from FILE *fp anymore
  fclose(fp);
 
  return 0;
